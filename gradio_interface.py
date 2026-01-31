51ca7c4d335c314eac66e5cbe4320212eea342c8
Al-Amin Islam Nerob <nerobit.786@gmail.com>
2026-02-01 02:22:24 +0600

    WIP

--- gradio_interface.py
+++ gradio_interface.py
@@ -1600,12 +1600,7 @@ def setup_gradio():
 
 def main():
     demo = setup_gradio()
-    demo.launch({
-        server_name="0.0.0.0",
-        server_port=7860,
-        share=False,
-        show_error=True
-    })
+    demo.launch()
 
 if __name__ == "__main__":
     import os
